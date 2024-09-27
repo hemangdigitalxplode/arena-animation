7fa402650531d0cf6a5b169f29e789bdde4080e4
hemangdigitalxplode <hemang@digitalxplode.com>
2024-09-27 19:02:07 +0530

    Home Page content updated

--- src/containers/subHeader/SubHeader.jsx
+++ src/containers/subHeader/SubHeader.jsx
@@ -13,38 +13,33 @@ const SubHeader = ({ bestanimationcms }) => {
           {/* <img src={Layer} alt="Image" className="Layer-Image" /> */}
           {/* <div className="container-default"> */}
           <Container>
-            {bestanimationcms && (
+            {/* {bestanimationcms && (
               <div dangerouslySetInnerHTML={{ __html: bestanimationcms }} />
-            )}
-            {/* <div className="heading-subheader">
+            )} */}
+
+
+            <div className="heading-subheader">
               THE BEST ANIMATION <br /> INSTITUTE <br /> IN DELHI NCR
             </div>
             <div className="paraContainer">
-              <div className="paraContainer-left">
+              <div className="paraContainer-left col-lg-7">
                 <div className="paraContainer-left-first">
-                  Arena Creative Campus established in the year 1996, offers
-                  world class training facilities in the field of Animation,
-                  Visual Fx, Gaming, Graphics and Web Designing.
+                Aptech Limited, one of India's biggest non-formal vocational training for over 30 years, created Arena Animation, the Best Animation Institute brand, in 1996. Arena Animation is a pioneer and a worldwide-known institute that provides non-formal academic training programs in animation alongside related topics.
                 </div>
                 <div className="paraContainer-left-second">
-                  Our innovative pedagogy focuses on fostering creative learning
-                  with the help of highly experienced, qualified and committed
-                  trainers who inspire students to strengthen their power of
-                  imagination and breathe life into their imaginary characters.
+                Arena Animation, with its strong industry connections and affiliations, has been producing relevant academic courses that impart skills that make students highly employable. Arena students work as Graphic Designers, Web Designers, 2D/3D Animators, 2D/3D Designers, AV Editors, Technical Trainers, 3D Modelers, Multimedia Programmers, Compositors, Visualizers, Content Developers, and Pre and Post Production Executives in major animation studios and entertainment companies.
                 </div>
               </div>
               <div className="paraContainer-right">
                 <div>
-                  It is the first centre to start its operations in South India
-                  and has now reached a stage where we can say Arena Animation
-                  students are the most preferred professionals in the industry.
+                The Best Institute for Animation in Delhi proudly maintains an amazing placement history spanning 25 years and continues to raise the standard for more successful professions!
                 </div>
-                <div className="knowMore">
+                {/* <div className="knowMore">
                   <div className="knowMore-text">KNOW MORE</div>
                   <img src={YellowArrow} className="arrow-knowMore" />
-                </div>
+                </div> */}
               </div>
-            </div> */}
+            </div>
 
             {/* </div> */}
             {/* </div> */}
